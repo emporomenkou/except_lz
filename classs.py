659932c1e40a1049bd91f327a619ed374a3dfd52
emporomenkou <noobprohacker@tutamail.com>
2026-05-12 21:07:48 +0300

    sdcv

---
+++ classs.py
@@ -0,0 +1,49 @@
+import pandas as pd
+
+b = 'var7.csv'
+class Data():
+    def __init__(self, a : str):
+        try:
+            self._df = pd.read_csv(a, sep=',')
+        except FileNotFoundError as e:
+            print(f"Возникла следующая ошибка: {str(e)}")
+        except pd.errors.EmptyDataError:
+            print(f"Возникла следующая ошибка: Датафрейм пуст")
+        else:
+            self._df = pd.read_csv(a, sep=',')
+            try:
+                cols = ['Участники гражданского оборота',
+                         'Тип операции',
+                         'Сумма операции',
+                           'Вид расчета',
+                             'Место оплаты',
+                               'Терминал оплаты',
+                                 'Дата оплаты',
+                                   'Время оплаты',
+                                     'Результат операции',
+                                       'Cash-back',
+                                         'Сумма cash-back']
+                if list(self._df.columns) != cols:
+                    raise ValueError(f"Названия стобцов не совпадают. \n Ожидаемые: {cols} \n Фактические: {list(self._df.columns)}")
+                cols_type = {'Участники гражданского оборота' : 'object',
+                             'Тип операции' : 'object',
+                             'Сумма операции' : 'float64',
+                             'Вид расчета' : 'object',
+                             'Место оплаты' : 'object',
+                             'Терминал оплаты' : 'object',
+                             'Дата оплаты' : 'object',
+                             'Время оплаты' : 'object',
+                             'Результат операции' : 'object',
+                             'Cash-back' : 'object',
+                             'Сумма cash-back' : 'float64'}
+                for c, type in cols_type.items():
+                    if self._df[c].dtype != type:
+                        raise TypeError(f"- В столбце '{c}' тип данных не соответствует ожидаемому. \n Ожидается: {cols_type[c]}, Фактически - {self._df[c].dtype}")
+            except ValueError as e:
+                print(str(e))
+            except TypeError as e:
+                print(str(e))
+def main():
+    df = Data(b)
+if __name__ == "__main__":
+    main()
---
+++ main.py
@@ -0,0 +1,3 @@
+import classs
+
+classs.main()
