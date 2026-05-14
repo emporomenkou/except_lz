5538299059019419f5373e0e1b54af91177294e3
Ваше Имяemporomenkou <noobprohacker@tutamail.com>
2026-05-14 09:52:08 +0300

    fgjfsn

--- classs.py
+++ classs.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from enum import Enum
 
 b = 'var7.csv'
 class Data():
@@ -10,39 +11,47 @@ class Data():
         except pd.errors.EmptyDataError:
             print(f"Возникла следующая ошибка: Датафрейм пуст")
         else:
-            self._df = pd.read_csv(a, sep=',')
+
             try:
+                self._df = pd.read_csv(a, sep=',')                
                 cols = ['Участники гражданского оборота',
-                         'Тип операции',
-                         'Сумма операции',
-                           'Вид расчета',
-                             'Место оплаты',
-                               'Терминал оплаты',
-                                 'Дата оплаты',
-                                   'Время оплаты',
-                                     'Результат операции',
-                                       'Cash-back',
-                                         'Сумма cash-back']
+                        'Тип операции',
+                        'Сумма операции',
+                        'Вид расчета',
+                        'Место оплаты',
+                        'Терминал оплаты',
+                        'Дата оплаты',
+                        'Время оплаты',
+                        'Результат операции',
+                        'Cash-back',
+                        'Сумма cash-back']
                 if list(self._df.columns) != cols:
                     raise ValueError(f"Названия стобцов не совпадают. \n Ожидаемые: {cols} \n Фактические: {list(self._df.columns)}")
-                cols_type = {'Участники гражданского оборота' : 'object',
-                             'Тип операции' : 'object',
+                cols_type = {'Участники гражданского оборота' : 'str',
+                             'Тип операции' : 'str',
                              'Сумма операции' : 'float64',
-                             'Вид расчета' : 'object',
-                             'Место оплаты' : 'object',
-                             'Терминал оплаты' : 'object',
-                             'Дата оплаты' : 'object',
-                             'Время оплаты' : 'object',
-                             'Результат операции' : 'object',
-                             'Cash-back' : 'object',
+                             'Вид расчета' : 'str',
+                             'Место оплаты' : 'str',
+                             'Терминал оплаты' : 'str',
+                             'Дата оплаты' : 'str',
+                             'Время оплаты' : 'str',
+                             'Результат операции' : 'str',
+                             'Cash-back' : 'str',
                              'Сумма cash-back' : 'float64'}
+                errors = []
                 for c, type in cols_type.items():
                     if self._df[c].dtype != type:
-                        raise TypeError(f"- В столбце '{c}' тип данных не соответствует ожидаемому. \n Ожидается: {cols_type[c]}, Фактически - {self._df[c].dtype}")
+                        k = 1
+                        errors += [f"- В столбце '{c}' тип данных не соответствует ожидаемому. \n Ожидается: {cols_type[c]}, Фактически - {self._df[c].dtype} \n", ]
+                if k == 1:
+                    raise TypeError
             except ValueError as e:
                 print(str(e))
-            except TypeError as e:
-                print(str(e))
+            except TypeError :
+                print(*(i for i in errors))
+            else:
+                self._df = pd.read_csv(a, sep=',')
+                print("Датафрейм обработан успешно")          
 def main():
     df = Data(b)
 if __name__ == "__main__":
